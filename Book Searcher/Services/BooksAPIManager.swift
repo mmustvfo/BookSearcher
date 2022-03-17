9dca02bf5c9c5c40cbf48ba5365aedbd37393d9f
mmustvfo <82271693+mmustvfo@users.noreply.github.com>
2022-03-17 15:25:14 +0500

    Data task is share, so when new one comes the old one gets disabled

--- Book Searcher/Services/BooksAPIManager.swift
+++ Book Searcher/Services/BooksAPIManager.swift
@@ -12,50 +12,68 @@ import Foundation
  Example: https://www.googleapis.com/books/v1/volumes?q=programming
  */
 
-class BooksAPIManager {
+protocol BooksAPI {
+    func makeRequest(for query: String,completion: @escaping(QueryResult?)-> Void)
+}
+
+class BooksAPIManager: BooksAPI {
     private let urlPath = "https://www.googleapis.com/books/v1/volumes?q="
     
     let defaultSession = URLSession(configuration: .default)
     
+    var dataTask: URLSessionDataTask?
+    
     static let shared = BooksAPIManager()
     
-    enum BooksAPIError: LocalizedError {
-        case invalidURL
-        case missingQuery
-        
-        var errorDescription: String? {
-            switch self {
-            case .invalidURL:
-               return "Invalid URL!"
-            case .missingQuery:
-                return "Missing query"
-            }
-        }
-    }
+    //MARK: TO DO
+//    enum BooksAPIError: LocalizedError {
+//        case invalidURL
+//        case missingQuery
+//        case parsingError
+//
+//        var errorDescription: String? {
+//            switch self {
+//            case .invalidURL:
+//               return "Invalid URL!"
+//            case .missingQuery:
+//                return "Missing query"
+//            case .parsingError:
+//                return "Couldn't parse"
+//            }
+//        }
+//    }
     
-    public func makeRequest(for query: String,completion: @escaping(Result<QueryResult,Error>)-> Void) {
+    public func makeRequest(for query: String,completion: @escaping(QueryResult?)-> Void) {
         
-        guard let url = generateURL(for: query) else {
-            print("URL INVALID")
-            completion(.failure(BooksAPIError.invalidURL))
+        if query.isEmpty {
+            completion(nil)
             return
         }
         
-        let dataTask = defaultSession.dataTask(with: url) { data, response, error in
-            guard let data = data, error == nil else {
+        guard let url = generateURL(for: query) else {
+            completion(nil)
+            return
+        }
+        
+        dataTask = defaultSession.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("ERROR: \(error)")
+                return
+            }
+            guard let data = data else {
                 print("DATA NIL")
                 return
             }
-            if let book = try? JSONDecoder().decode(QueryResult.self, from: data) {
-                completion(.success(book))
-                print("Count: \(book.items.count)")
+            if let queryResult = try? JSONDecoder().decode(QueryResult.self, from: data) {
+                completion(queryResult)
+                print("Count: \(queryResult.items.count)")
             } else {
-                print("COULDN'T PARSE")
                 print(data)
+                completion(nil)
             }
         }
         
-        dataTask.resume()
+        dataTask?.resume()
     }
     
     private func generateURL(for query: String)-> URL? {
